6b5a3639be37ba463ddbc6734c71f478afd9159e
IlyaKhokhanov <hohanov.ilya@gmail.com>
2024-06-14 12:25:33 +0300

    feat: install react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
-    <div>
-      <h1>Pharmacy</h1>
-    </div>
+    <HashRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
